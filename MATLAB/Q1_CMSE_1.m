a8075937abd3f2e45fafd36f4f1c37b7b6a30977
adehad <26027314+adehad@users.noreply.github.com>
2019-02-05 21:55:10 +0000

    Initial Assignment 1 commit
    Added script with init and raw data files needed

---
+++ MATLAB/Q1_CMSE_1.m
@@ -0,0 +1,13 @@
+%% Q1 Classical and Modern Spectrum Estimation
+%% Premable
+% Use Ctrl+Enter to run code section by section
+
+% Clear out the workspace
+clc; clear all;
+
+% Initialise common functions for this question
+questionNum = 1;
+q_initialise;
+
+
+%% 
