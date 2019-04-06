f237d7bed64157160ef2865cac02ae26e71c9d02
adehad <26027314+adehad@users.noreply.github.com>
2019-04-06 18:48:21 +0100

    init q5
    started a bit of section 1

---
+++ Python/runQ5.bat
@@ -0,0 +1,5 @@
+setlocal
+set PATH=%PATH%;C:\Conda\Miniconda3\envs\phy\Scripts
+start cmd.exe /k "C:\Conda\Miniconda3\Scripts\activate.bat dpm-coursework" 
+
+rem run: jupyter notebook notebooks\XYZ>.ipynb 
